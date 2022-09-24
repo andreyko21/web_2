9165d40d011f5c6e5c16c6bf7bdb074a9f97643c
andreyko21 <112313332+andreyko21@users.noreply.github.com>
2022-09-24 18:52:00 +0300

    v

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
 new Swiper('.our-partners', {
-    slidesPerView: 4,
     loop:true,
+    spaceBetween: 30,
     autoplay: {
         delay: 2000,
+    },
+    breakpoints: {
+        1200: {
+            slidesPerView: 4,
+        },
+        900: {
+            slidesPerView: 3,
+        },
+        700: {
+            slidesPerView: 2,
+        }
     }
 });
 
@@ -10,13 +21,14 @@ new Swiper('.season__watch',{
     watchOverflow: true,
     centeredSlides: true,
     slidesPerView: 3,
+    spaceBetween: 30,
     loop:true,
     autoplay: {
         delay: 2000,
     }
 });
 
-    new Swiper('.list__watch',{
+    new Swiper('.watchs-list',{
         watchOverflow: true,
         centeredSlides: true,
         slidesPerView: 3,
